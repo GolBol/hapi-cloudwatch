2edac0944e138cd161b08181ef532c94025ef705
Zoltan Komives <zolaemil@users.noreply.github.com>
2019-09-17 09:36:39 +0100

    refactor: for hapi@17

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const packageJSON = require('./package.json');
 const lifecycle = require('./lib/lifecycle');
 const metric = require('./lib/metric');
 
-exports.register = (server, options, next) => {
-  const responseMetric = metric.create(options);
-
-  server.ext('onRequest', lifecycle.setStartTime);
-  server.ext('onPreResponse', lifecycle.setEndTime);
-
-  server.on('response', request => {
-    const dimensions = metric.createDimensions(request);
-    responseMetric.put(request.app.endTime - request.app.startTime, 'responseTime', dimensions);
-  });
-
-  return next();
-};
-
-exports.register.attributes = {
+exports.plugin = {
   pkg: packageJSON,
+  name: 'hapi-cloudwatch',
+
+  async register(server, options) {
+    const responseMetric = metric.create(options);
+
+    server.ext('onRequest', lifecycle.setStartTime);
+    server.ext('onPreResponse', lifecycle.setEndTime);
+
+    server.events.on('response', request => {
+      const dimensions = metric.createDimensions(request);
+      responseMetric.put(request.app.endTime - request.app.startTime, 'responseTime', dimensions);
+    });
+  },
 };
