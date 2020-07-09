966f7472120a5eb5064722c758ae6d48e6a781eb
kaushik.golbol <kaushik@golbol.in>
2020-07-09 13:25:38 +0530

    Add Request & Response Count Metric

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ exports.plugin = {
   name: 'hapi-cloudwatch',
 
   async register(server, options) {
-    const responseMetric = metric.create(options);
+    const responseTimeMetric = metric.createResponseTimeMetric(options);
+	const countMetric = metric.createCountMetric(options);
 
     server.ext('onRequest', lifecycle.setStartTime);
     server.ext('onPreResponse', lifecycle.setEndTime);
 
+	server.events.on('request', request => {
+      const dimensions = metric.createRequestDimensions(request);
+      countMetric.put(1, 'RequestCount', dimensions);
+    });
+	
     server.events.on('response', request => {
-      const dimensions = metric.createDimensions(request);
-      responseMetric.put(request.app.endTime - request.app.startTime, 'ApiResponseTime', dimensions);
+      const dimensions = metric.createResponseDimensions(request);
+      responseTimeMetric.put(request.app.endTime - request.app.startTime, 'ApiResponseTime', dimensions);
+	  countMetric.put(1, 'ResponseCount', dimensions);
     });
   },
 };
